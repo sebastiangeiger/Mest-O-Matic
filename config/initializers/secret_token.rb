0a45608aa23988df1e885db60434f2c8dda69011
Sebastian Geiger <sebastian@geiga.net>
2011-05-04 12:58:31 +0000

    Executed the rails new command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MestOMatic::Application.config.secret_token = '2bc01415b57d34f31b2b7eb4881eef43d50e99a660f8a1a45aeff8205f74a5c1d267e5e6d380e7a4bc62d67480c5874b758bf0706ec8b7d016a2e1e7aef66925'
