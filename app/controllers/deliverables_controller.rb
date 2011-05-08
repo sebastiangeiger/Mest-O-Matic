01ab2efcc8641081e1bfcbcf0dc8ca47b76a41b1
Sebastian Geiger <sebastian@geiga.net>
2011-05-08 22:51:31 +0000

    Added specs for deliverables controller.

--- app/controllers/deliverables_controller.rb
+++ app/controllers/deliverables_controller.rb
@@ -3,8 +3,6 @@ class DeliverablesController < ApplicationController
   
   def load_project_and_deliverables
     @project = Project.find(params[:project_id])
-    @deliverables = Deliverable.find_by_project_id(params[:project_id])
-    puts "Loaded project and deliverables"
   end
 
   def new
@@ -14,10 +12,9 @@ class DeliverablesController < ApplicationController
   def create
     @deliverable = Deliverable.new(params[:deliverable])
     @project.deliverables << @deliverable
-    if params[:commit].eql?("cancel") 
+    if params[:commit].eql?("cancel") or @deliverable.save
+      flash[:notice] = "Successfully created the deliverable #{@deliverable.title}" if @deliverable.save
       redirect_to project_path(@project) 
-    elsif @deliverable.save
-      redirect_to project_path(@project), :notice => "Successfully created the deliverable #{@deliverable.title}"
     else
       @project.deliverables.delete(@deliverable)
       render :action => "new"
---
+++ spec/controllers/deliverables_controller_spec.rb
@@ -0,0 +1,57 @@
+require_relative '../spec_helper.rb'
+
+describe DeliverablesController do
+  render_views
+  
+  before(:each) do
+    @project = Project.new(:title => "Project from database")
+    @project.stubs(:id).returns(11)
+  end
+
+  describe "responding to GET new" do
+    it "should create a new deliverable and load the project, the render the new template" do
+      Project.expects(:find).with(11).returns @project
+      get :new, :project_id => 11
+      assigns[:deliverable].should_not be_nil
+      assigns[:deliverable].should be_new_record
+      assigns[:project].should == @project
+      response.should render_template("deliverables/new")
+    end
+  end
+  
+  describe "responding to POST create" do
+    describe "user clicked cancel button" do
+      it "should redirect to project path and do nothing otherwise"
+    end
+    
+    describe "with valid parameters" do
+      it "should create a new deliverable that is part of the current project" do      
+        Deliverable.any_instance.stubs(:valid?).returns(true)
+        Project.expects(:find).with(11).returns @project
+        post :create, :project_id => 11
+        assigns[:project].should == @project
+        assigns[:deliverable].should 
+        assigns[:project].deliverables.should include(assigns[:deliverable])
+        assigns[:deliverable].should_not be_new_record
+        response.should redirect_to(project_path(@project))
+        flash[:notice].should_not be_nil
+      end
+      
+      it "should pass the params to the deliverable item" #do
+      #   post :create, "deliverable" => {:title => "Some Deliverable"}
+      #   assigns[:deliverable].title.should == "Some Deliverable"
+      # end
+    end
+    describe "with invalid parameters" do
+      it "should render the new template" do
+        Deliverable.any_instance.stubs(:valid?).returns(false)
+        Project.expects(:find).with(11).returns @project
+        post :create, :project_id => 11
+        response.should render_template("deliverables/new")
+        assigns[:project].deliverables.should_not include(assigns[:deliverable])
+        assigns[:deliverable].should be_new_record
+        flash[:notice].should be_nil
+      end      
+    end
+  end
+end
