ff4dedd389b4cfd1fe6e0b67016a319dd6295c4d
Sebastian Geiger <sebastian@geiga.net>
2011-05-08 18:35:37 +0000

    Validations and tests for deliverable.

--- app/models/deliverable.rb
+++ app/models/deliverable.rb
@@ -14,5 +14,16 @@
 #
 
 class Deliverable < ActiveRecord::Base
-  belongs_to :projects
+  belongs_to :project
+  
+  validates :title, :presence => true
+  validates_uniqueness_of :title, :scope => :project_id, :case_sensitive => false
+  validates :project, :presence => true
+  validates :start_date, :presence => true
+  validates :end_date, :presence => true
+  validate :start_date_must_be_before_end_date
+  
+  def start_date_must_be_before_end_date
+    errors.add(:end_time, 'start date must be before end date') if start_date and end_date and start_date >= end_date
+  end
 end
---
+++ spec/models/deliverable_spec.rb
@@ -0,0 +1,59 @@
+require_relative '../spec_helper.rb'
+
+describe Deliverable do
+  before(:each) do
+    @project = Project.new
+    @project.stub(:id).and_return(1)
+    @project_2 = Project.new
+    @project_2.stub(:id).and_return(2)
+  end
+  
+  it "should create a deliverable with a title, a project and start/end dates" do
+    Deliverable.create(:title => "Some deliverable", :project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should be_valid
+  end
+  
+  it "should not create a deliverable without a title" do
+    Deliverable.create(:project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should_not be_valid    
+  end
+
+  it "should not create a deliverable without a project" do
+    Deliverable.create(:title => "Some deliverable", :start_date => Time.now-1.day, :end_date => Time.now).should_not be_valid    
+  end
+
+  it "should not create a deliverable without a start date" do
+    Deliverable.create(:title => "Some deliverable", :project => @project, :end_date => Time.now).should_not be_valid    
+  end
+
+  it "should not create a deliverable without a end date" do
+    Deliverable.create(:title => "Some deliverable", :project => @project, :start_date => Time.now-1.day).should_not be_valid    
+  end
+
+  it "should create two deliverables with the same title in different projects" do
+    Deliverable.create(:title => "Some deliverable", :project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should be_valid
+    Deliverable.create(:title => "Some deliverable", :project => @project_2, :start_date => Time.now-1.day, :end_date => Time.now).should be_valid
+  end
+
+  it "should not create two deliverables with the same title in the same project" do
+    Deliverable.create(:title => "Some deliverable", :project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should be_valid
+    Deliverable.create(:title => "Some deliverable", :project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should_not be_valid
+  end
+
+  it "should not create two deliverables with the same title in the same project (ignoring case)" do
+    Deliverable.create(:title => "Some deliverable", :project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should be_valid
+    Deliverable.create(:title => "SOME DELIVERABLE", :project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should_not be_valid
+  end
+
+  it "should create two deliverables with the different titles in the same project" do
+    Deliverable.create(:title => "Some deliverable", :project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should be_valid
+    Deliverable.create(:title => "SOME other DELIVERABLE", :project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should be_valid
+  end
+  
+  it "should create a deliverable if the start date is before the end date" do
+    Deliverable.create(:title => "Some deliverable", :project => @project, :start_date => Time.now-1.day, :end_date => Time.now).should be_valid    
+  end
+
+  it "should not create a deliverable if the start date is after the end date" do
+    Deliverable.create(:title => "Some deliverable", :project => @project, :start_date => Time.now+1.day, :end_date => Time.now).should_not be_valid    
+  end
+
+end
