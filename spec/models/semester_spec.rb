223afd2f414a573ab76ce7b94a3179c77aff65e2
Sebastian Geiger <sebastian@geiga.net>
2011-05-08 18:35:53 +0000

    Fixed spec for semester model.

--- spec/models/semester_spec.rb
+++ spec/models/semester_spec.rb
@@ -1,12 +1,19 @@
 require_relative '../spec_helper.rb'
 
 describe Semester do
+  before(:each) do
+    @class_of = ClassOf.new
+    @class_of.stub(:id).and_return(1)
+    @class_of_2 = ClassOf.new
+    @class_of.stub(:id).and_return(2)
+  end
+
   it "should create a semester with a class_of and a nr" do
-    Semester.create(:class_of => ClassOf.new, :nr => 1).should be_valid
+    Semester.create(:class_of => @class_of, :nr => 1).should be_valid
   end
 
   it "should not create a semester with only a class_of" do
-    Semester.create(:class_of => ClassOf.new).should_not be_valid
+    Semester.create(:class_of => @class_of).should_not be_valid
   end
 
   it "should not create a semester with only a nr" do
@@ -18,14 +25,17 @@ describe Semester do
   end
 
   it "should create two semesters with same class of but different nrs" do
-    class_of = ClassOf.new()
-    Semester.create(:class_of => class_of, :nr => 1).should be_valid
-    Semester.create(:class_of => class_of, :nr => 2).should be_valid    
+    Semester.create(:class_of => @class_of, :nr => 1).should be_valid
+    Semester.create(:class_of => @class_of, :nr => 2).should be_valid    
+  end
+
+  it "should create two semesters with same nrs of but for different clases" do
+    Semester.create(:class_of => @class_of, :nr => 1).should be_valid
+    Semester.create(:class_of => @class_of_2, :nr => 1).should be_valid    
   end
 
   it "should not create two semesters with same class and same nrs" do
-    class_of = ClassOf.new()
-    Semester.create(:class_of => class_of, :nr => 1).should be_valid
-    Semester.create(:class_of => class_of, :nr => 1).should_not be_valid    
+    Semester.create(:class_of => @class_of, :nr => 1).should be_valid
+    Semester.create(:class_of => @class_of, :nr => 1).should_not be_valid    
   end
 end
