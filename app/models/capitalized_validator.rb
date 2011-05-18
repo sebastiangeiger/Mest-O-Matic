73de62c5038b146fb54d05fd6f9ce1ef09af2446
Sebastian Geiger <sebastian@geiga.net>
2011-05-18 18:56:20 +0000

    Fixed problem with double-names and capitalization validator

--- app/models/capitalized_validator.rb
+++ app/models/capitalized_validator.rb
@@ -2,7 +2,7 @@ class CapitalizedValidator < ActiveModel::EachValidator
   # implement the method called during validation
   def validate_each(record, attribute, value)
     if value then
-      fractions = value.split(" ").reject{|e| e.empty?}
+      fractions = value.split(/ |-/).reject{|e| e.empty?}
       record.errors[attribute] << 'must start with capital letter' unless fractions.inject(true){|mem, el| mem &= el.eql?(el.capitalize)}
     end
   end
