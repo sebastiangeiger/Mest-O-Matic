fb99d76bcef351a0d1cb7f4e57ee5a6affd03ef7
Sebastian Geiger <sebastian@geiga.net>
2011-08-15 11:08:21 +0200

    Redirecting to projects is not feasible, showing a link instead.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    redirect_to projects_path
   end
 
 end
--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -1,15 +1,12 @@
 require 'spec_helper'
 
 feature "Welcome", %q{
-  Shows empty page with login button
+  Shows a link to projects page and a login button
 } do
-
-  background do
-    p User.all
-  end
 
   scenario "Welcome index" do
     visit "/"
+    page.should have_link("Assignments")
     page.should have_content("login")
   end
 
